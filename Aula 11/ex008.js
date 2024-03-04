b7d0659cad4671ba92c3db9e5fc3b746d4f6a3f1
Breno <159046713+BrenoAlvesTI@users.noreply.github.com>
2024-03-04 13:06:54 -0300

    estudos

---
+++ Aula 11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel <= 60) {
+    console.log('Dirija com cuidado!')
+}else {
+    console.log('Você ultrapassou o limite de velocidade permitido. MULTADO!')
+}
